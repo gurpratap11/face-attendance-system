1b334edcec01e495064a70e655cd0a39e03a39e5
priyxnshuuu <priyanshushukla1217@gmail.com>
2023-10-21 15:58:29 +0530

    remove production

--- backend/src/env/Env.ts
+++ backend/src/env/Env.ts
@@ -1,5 +1,4 @@
 import envDev from "./env.dev";
-import envProd from "./env.prod";
 
 export class Env {
   env: Environment;
@@ -12,8 +11,6 @@ export class Env {
     switch (this.env) {
       case "dev":
         return envDev;
-      case "prod":
-        return envProd;
       case "local":
         return envDev;
       default:
